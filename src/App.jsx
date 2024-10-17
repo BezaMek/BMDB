bfea252b388e6080c4cb801da40ab8ae93154a08
bmovie92 <bezam123456@gmail.com>
2024-10-17 12:50:47 +0300

    adjusted sign up2

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,12 @@
-import {
-  createBrowserRouter,
-  RouterProvider} from "react-router-dom";
-  import { useParams} from "react-router-dom";
-  import { useEffect ,useState} from "react";
-import Header from "./components/Header.jsx"
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import Header from "./components/Header.jsx";
 import Login from "./components/Login.jsx";
 import Welcome from "./components/Welcome.jsx";
 import Trending from "./components/Trending.jsx";
 import MovieDetail from "./components/Movie/MovieDetail.jsx";
- 
- 
+
 import LeaderBord from "./components/LeaderBord.jsx";
 import Footer from "./components/Footer.jsx";
 
@@ -17,41 +14,36 @@ import Popular from "./components/Popular.jsx";
 import JoinToday from "./components/JoinToday.jsx";
 
 import SearchPage from "./components/SearchPage.jsx";
-import SignUp from "./components/signUp.jsx";
+import SignUp from "./components/Signup.jsx";
 export default function App() {
- 
-const [selected, setSelected]= useState('movie')
-const handleClick = (type) => {
-  setSelected(type);
-};
+  const [selected, setSelected] = useState("movie");
+  const handleClick = (type) => {
+    setSelected(type);
+  };
   const router = createBrowserRouter([
     {
       path: "/",
-      element:(
-        <> 
-         <Header />
-       <Welcome/>
-    
-         <Trending/>
-         {/* <Trailer/>  */}
-       <Popular/> 
-       <JoinToday/>
-        <LeaderBord/> 
-        <Footer/>  
-     
+      element: (
+        <>
+          <Header />
+          <Welcome />
 
-      
-       </>
-      
-      ) 
+          <Trending />
+          {/* <Trailer/>  */}
+          <Popular />
+          <JoinToday />
+          <LeaderBord />
+          <Footer />
+        </>
+      ),
     },
     {
-      path:"Login",
-      element: <Login/>
+      path: "Login",
+      element: <Login />,
     },
     {
-      path:"SignUp",
-      element: <SignUp/>
+      path: "SignUp",
+      element: <SignUp />,
     },
     {
       path: `/${selected}/:id`,
@@ -72,21 +64,14 @@ const handleClick = (type) => {
       ),
     },
     {
-      path:"Popup",
-      element:<MovieDetail/> 
+      path: "Popup",
+      element: <MovieDetail />,
     },
     {
-      path:"search",
-      element:<SearchPage/>
-    }
+      path: "search",
+      element: <SearchPage />,
+    },
   ]);
 
   return <RouterProvider router={router} />;
-  
-    
- 
-    
-  
 }
-
- 
